124612384143f606e4827e273cba8857eb48f21c
kubilayciftci <kubilayciftci@outlook.com>
2021-04-01 19:30:03 +0300

    get request with axios

--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const UserContext = React.createContext();
 
@@ -12,19 +13,26 @@ const reducer = (state, action) => {
         case "ADD_USER":
             return {
                 ...state,
-                users: [...state.users,action.payload]
+                users: [...state.users, action.payload]
             }
         default:
             return state;
     }
 }
-
 export class UserProvider extends Component {
     state = {
         users: [],
         dispatch: action => {
             this.setState(state => reducer(state, action));
         }
+    }
+
+    componentDidMount = async () => {
+        const response = await axios.get("http://localhost:3004/users");
+        console.log(response);
+        this.setState({
+            users: response.data
+        })
     }
 
     render() {
